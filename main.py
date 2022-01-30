a912d81a2f7ce07d507a56f69bbe26b9ba1a556a
KrKOo <krkoo.123@gmail.com>
2022-01-30 22:25:48 +0100

    async queue and refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from discord.utils import get
 from discord import FFmpegPCMAudio
 from discord import TextChannel
 from youtube_dl import YoutubeDL
-from urllib.parse import urlparse 
+from urllib.parse import urlparse
 from urllib.parse import parse_qs
 import validators
 import subprocess
@@ -19,45 +19,54 @@ load_dotenv()
 client = commands.Bot(command_prefix='.')  # prefix our commands with '.'
 
 players = {}
-q = []
+musicQueue = []
+playNext = asyncio.Event()
 
-#def remFromQ(number):
+# def remFromQ(number):
+
 
 def is_connected(ctx):
     voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
     return voice_client and voice_client.is_connected()
 
+
 def is_playing(ctx):
     voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
     return voice_client.is_playing()
 
-def addToQ(url):
-    q.append(url)
+
+def addToQueue(url):
+    musicQueue.append(url)
+
 
 def is_playlist(url):
     parsed = urlparse(url)
     captured_value = parse_qs(parsed.query)
     if 'list' in captured_value:
-        output = subprocess.run(os.system('youtube-dl -j --flat-playlist '+url), capture_output=True)
+        output = subprocess.run(
+            os.system('youtube-dl -j --flat-playlist '+url), capture_output=True)
 
 
-def checkQ(ctx, voice):
+async def playQueue(ctx, voice):
     YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-    if not is_playing(ctx) and q:
-        url = q[0]
-        q.pop(0)
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    while len(musicQueue) > 0:
+        url = musicQueue.pop(0)
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
-        URL = info['url']
-        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS), after = lambda e: checkQ(ctx, voice))
+        url = info['url']
 
-        
-    else:
-        #ctx.send('Q
-        # is empty. FEED ME WITH MOAR MUSIC :slight_smile:')
-        return
-    
+        playNext.clear()
+        voice.play(FFmpegPCMAudio(url, **FFMPEG_OPTIONS),
+                   after=lambda _: playNext.set())
+
+        # Wait for the song to finish
+        await playNext.wait()
+        #print("DONE PLAYING")
+
+
 @client.event  # check if bot is ready
 async def on_ready():
     print('Bot online')
@@ -66,7 +75,6 @@ async def on_ready():
 # command for bot to join the channel of the user, if the bot has already joined and is in a different channel, it will move to the channel the user is in
 @client.command(pass_context=True)
 async def join(ctx):
-
     voice = get(client.voice_clients, guild=ctx.guild)
     if(ctx.author.voice):
         channel = ctx.message.author.voice.channel
@@ -77,45 +85,36 @@ async def join(ctx):
     else:
         await ctx.send('YO NOT CONNECTED')
 
-# command to play sound from a youtube URL
 
+# command to play sound from a youtube URL
 @client.command()
 async def play(ctx, url):
     YDL_OPTIONS = {'format': 'bestaudio'}
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
     if not validators.url(url):
         await ctx.send('Not a valid URL')
         return
-    
-    
+
     if not is_connected(ctx):
         await ctx.send(':slight_smile:')
         await join(ctx)
+
     voice = get(client.voice_clients, guild=ctx.guild)
-    print(url)
-    is_playlist(url)
+
+    addToQueue(url)
+    # print(list(musicQueue))
+
     if not is_playing(ctx):
-        with YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-        URL = info['url']
-        
-        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS), after = lambda e: checkQ(ctx, voice))
-        voice.is_playing()
+        await playQueue(ctx, voice)
         await ctx.send('Bot is playing')
-
-
-# check if the bot is already playing
-    else:
-        addToQ(url)
-        print(type(q), type(ctx))
-        print(q)
 
 
 @client.command()
 async def queue(ctx):
     await ctx.send('Current queue: ')
-    await ctx.send(q)
-
+    await ctx.send(musicQueue)
 
 
 # command to resume voice if it is paused
@@ -154,10 +153,11 @@ async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
     await ctx.send("Messages have been cleared")
 
+
 @client.command()
 async def leave(ctx):
     if (ctx.voice_client):
-        await ctx.guild.voice_client.disconnect() # Leave the channel
+        await ctx.guild.voice_client.disconnect()  # Leave the channel
         await ctx.send('Idzem')
     else:
         await ctx.send('Fok of i ain\'t connected :slight_smile:')
